e0efca5fc7af43b62c8252b3b33812e171bcd9f9
mariquita5 <majomadrig5@gmail.com>
2016-11-10 20:04:32 -0600

    +cambios

---
+++ src/dotnetstarter/bin/Debug/netcoreapp1.0/wwwroot/Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnethelloworld.wwwroot
+{
+    public class Class
+    {
+public void btnEliminar_Click(object sender, EventArgs e)
+
+{
+
+ //el código que queramos ejecutar
+
+}
+    }
+}
